f30689124d773f50978d5d53ecbe95377260a74d
Fagner-Cardoso <e.commandtech@gmail.com>
2024-06-09 23:07:45 -0300

    Update adivinhacao.py com loop while

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,32 +4,41 @@ print("*     Jogo da adivinhação      *")
 print("********************************")
 
 
-# definição variável número secreto 
-
+# definição variável número secreto e quantidade de tentativas 
+rodada = 1
+total_de_tentativas = 3
 numero_secreto = 10
 
 
-# área de imput 
+# inicio do loop
 
-chute = int(input("Digite o número escolhido:\n"))
-print("O número digitado foi:", chute)
+while(total_de_tentativas > 0):
+ 
+  print("Rodada {} de {} tentativas" .format(rodada, total_de_tentativas))
+
+# área de imput 
+  chute = int(input("Digite o número escolhido:\n"))
+  print("O número digitado foi:", chute)
 
 # variáveis de estado do jogo
-
-venceu = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+  venceu = chute == numero_secreto
+  maior = chute > numero_secreto
+  menor = chute < numero_secreto
 
 # condicional do jogo
 
-if(venceu):
- print("Parabéns! Você acertou.\n")
-elif(maior):
- print("Você errou! O seu chute foi maior que o número secreto.\n")
-elif(menor):
- print("Você errou! O seu chute foi menor que o número secreto.\n")
+  if(venceu):
+    print("Parabéns! Você acertou.\n")
+    break
+  elif(maior):
+    print("Você errou! O seu chute foi maior que o número secreto.\n")
+  elif(menor):
+    print("Você errou! O seu chute foi menor que o número secreto.\n")
 
+  rodada += 1	
+  total_de_tentativas -= 1
 
+#fim do loop
 
 # texto final
 print("Fim de jogo.") 
